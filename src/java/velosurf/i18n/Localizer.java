298a65b4522f4bacfd3d175c3a16ca5222476f29
Claude Brisson <arkanovicz@users.noreply.github.com>
2006-09-17 09:52:14 +0000

    rename local to i18n

---
+++ src/java/velosurf/i18n/Localizer.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2003 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package velosurf.local;
+
+import java.util.Locale;
+
+/** To be implemented by localizers used by the engine to get localized strings from their database id.
+ *
+ *  <a href=mailto:claude.brisson.com>Claude Brisson</a>
+ *
+ **/
+
+public interface Localizer {
+    public Locale getLocale();
+    public String get(Object id);
+//    public Object getId(String text);
+}
