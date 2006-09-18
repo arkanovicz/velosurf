d73206b8064930afb5f880499c280f1776e556ec
clood <clood@aef81ea2-f21c-0410-b0e4-e2c0261c1d7f>
2006-09-18 15:39:56 +0000

    debuging...

--- trunk/src/java/velosurf/i18n/HTTPLocalizerTool.java
+++ trunk/src/java/velosurf/i18n/HTTPLocalizerTool.java
@@ -14,12 +14,13 @@
  * limitations under the License.
  */
 
-package velosurf.local;
+package velosurf.i18n;
 
 import org.apache.velocity.tools.view.context.ViewContext;
 import org.apache.velocity.tools.view.tools.ViewTool;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.ServletContext;
 import java.util.*;
 
 import velosurf.util.Logger;
@@ -45,36 +46,128 @@ public abstract class HTTPLocalizerTool implements Localizer,ViewTool {
     public void configure(Map parameters) {
         String def = (String)parameters.get("default-locale");
         if (def != null) {
-            _defaultLocale = def;
+            synchronized(_defaultLocale) {
+                _defaultLocale = def; // TODO : this static parameter is initialized at each session creation !!!
+            }
         }
         Logger.info("Localizer: using default locale: "+_defaultLocale);
     }
 
     public void init(Object initData) {
+        HttpServletRequest request = null;
         if (initData instanceof ViewContext) {
-            HttpServletRequest request = ((ViewContext)initData).getRequest();
-            Enumeration locales = request.getLocales();
-            _localeList = new ArrayList<Locale>();
-            while(locales.hasMoreElements()) {
-                _localeList.add((Locale)locales.nextElement());
-            }
-            /* always add the default locale afterwards */
-            _localeList.add(new Locale(_defaultLocale));
-        } else {
-            Logger.error("Localizer tool should be used in a session scope!");
+            request = ((ViewContext)initData).getRequest();
+            // for now... TODO
+            _locale = (Locale)request.getSession(false).getAttribute("active-locale");
         }
+        else {
+            Logger.error("Localizer tool should be used in a session scope!");
+            return;
+        }
+        _localeList = getRequestedLocales(request);
     }
 
-    public List<Locale> getLocaleList() {
-        return _localeList ;
+    public static Locale parseLocale(String candidate) {
+        if (_supportedLocalesCache == null) {
+            Logger.error("HTTPLocalizerTool.parseLocale(): I don't know supported locales!");
+            return null;
+        }
+        for(Locale locale:_supportedLocalesCache) {
+            if (locale.toString().equals(candidate)) {
+                return locale;
+            }
+        }
+        return null;
+    }
+
+    public static List<Locale> getRequestedLocales(HttpServletRequest request) {
+        List<Locale> list = new ArrayList<Locale>();
+        Enumeration locales = request.getLocales();
+        list = new ArrayList<Locale>();
+        while(locales.hasMoreElements()) {
+            list.add((Locale)locales.nextElement());
+        }
+        /* always add the default locale afterwards */
+        list.add(new Locale(_defaultLocale));
+        return list;
+    }
+
+    public static synchronized List<Locale> getSupportedLocales(ServletContext ctx,String path) {
+        if (_supportedLocalesCache != null) return _supportedLocalesCache;
+        List<Locale> locales = new ArrayList<Locale>();
+        String languages[] = Locale.getISOLanguages();
+        String countries[] = Locale.getISOCountries();
+        Arrays.sort(languages);
+        Arrays.sort(countries);
+        String language,country;
+        for(String resource:(Set<String>)ctx.getResourcePaths(path)) {
+            /* first, it must be a path */
+            if (resource.endsWith("/")) {
+                int len = resource.length();
+                int i = resource.lastIndexOf('/',len-2);
+                String locale = resource.substring(i+1,len-1);
+                if((i=locale.indexOf('_'))!=-1) {
+                    language = locale.substring(0,i);
+                    country = locale.substring(i+1);
+                } else {
+                    language = locale;
+                    country = null;
+                }
+                /* then it must contains valid language and country codes */
+                if (Arrays.binarySearch(languages,language) >= 0
+                        && (country == null || Arrays.binarySearch(countries,country) >= 0 )) {
+                    /* looks ok... */
+                    locales.add(country == null ? new Locale(language) : new Locale(language,country));
+                }
+            }
+        }
+        _supportedLocalesCache = locales;
+        return locales;
+    }
+
+    public static Locale getBestMatchingLocale(List<Locale> requestedLocales, List<Locale> supportedLocales) {
+        for(Locale locale:requestedLocales) {
+            if(supportedLocales.contains(locale)) {
+                return locale;
+            }
+        }
+        /* still there? Ok, second pass without the country. */
+        for(Locale locale:requestedLocales) {
+            if (locale.getCountry() != null) {
+                locale = new Locale(locale.getLanguage());
+                if(supportedLocales.contains(locale)) {
+                    return locale;
+                }
+            }
+        }
+        /* Oh, well... */
+        return new Locale(_defaultLocale);
+    }
+
+    public static Locale getDefaultLocale() {
+        return new Locale(_defaultLocale);
+    }
+
+    /**
+     * Meant to be overloaded.
+     * @return the list of supported locales
+     */
+    public List<Locale> getSupportedLocales() {
+        List<Locale> list = new ArrayList<Locale>();
+        list.add(Locale.ENGLISH);
+        return list;
     }
 
     public Locale getLocale() {
-        return _localeList.get(0);
+        return _locale;
     }
 
     public abstract String get(Object id);
 
     protected List<Locale> _localeList = null;
-    protected String _defaultLocale = "en";
+
+    protected Locale _locale = null;
+
+    protected static String _defaultLocale = "en";
+    protected static List<Locale> _supportedLocalesCache = null;
 }
