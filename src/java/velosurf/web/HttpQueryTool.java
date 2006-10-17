aaa845633f2201dfe44a99ec0fc1b2163622ddb2
Claude Brisson <arkanovicz@users.noreply.github.com>
2006-10-17 15:28:50 +0000

    HttpQueryTool now inherits from org.apache.Velocity.tools.view.tools.ParameterParser

--- src/java/velosurf/web/HttpQueryTool.java
+++ src/java/velosurf/web/HttpQueryTool.java
@@ -35,7 +35,7 @@ import velosurf.model.Entity;
  *  @author <a href=mailto:claude.brisson.com>Claude Brisson</a>
  *
  **/
- public class HttpQueryTool extends ParameterParser
+ public class HttpQueryTool extends ParameterParser implements Map
 {
 
     private ViewContext context = null;
@@ -80,23 +80,77 @@ import velosurf.model.Entity;
         }
     }
 
-    public Object get(String key)
+    public Object get(Object key)
     {
         Object ret = extraValues.get(key);
         if (ret == null) {
-            return super.get(key);
+            return super.get(key.toString());
         } else {
             return ret;
         }
     }
 
-    public Object put(String key, Object value) {
+    public Object put(Object key, Object value) {
         return extraValues.put(key,value);
     }
 
     public static void addAutofetch(Entity entity,String param,String name,boolean protect) {
         sAutofetchingEnabled = true;
         sAutofetchMap.put(param,new AutoFetchInfos(entity,name,protect));
+    }
+
+    public int size() {
+        return context.getRequest().getParameterMap().size() + extraValues.size();
+    }
+
+    public boolean isEmpty() {
+        return context.getRequest().getParameterMap().isEmpty() && extraValues.isEmpty();
+    }
+
+    public boolean containsKey(Object key) {
+        return context.getRequest().getParameterMap().containsKey(key) || extraValues.containsKey(key);
+    }
+
+    public boolean containsValue(Object value) {
+        String[] array = new String[1];
+        array[0] = (String)value;
+        return context.getRequest().getParameterMap().containsValue(array) || extraValues.containsValue(value);
+    }
+
+    public Object remove(Object key) {
+        return extraValues.remove(key);
+    }
+
+    public void putAll(Map map) {
+        extraValues.putAll(map);
+    }
+
+    public void clear() {
+        extraValues.clear();
+    }
+
+    public Set keySet() {
+        Set ret = context.getRequest().getParameterMap().keySet();
+        ret.addAll(extraValues.keySet());
+        return ret;
+    }
+
+    public Collection values() {
+        String[] array;
+        Collection ret = new HashSet();
+        Collection coll = context.getRequest().getParameterMap().values();
+        for(Object value:coll) {
+            array=(String[])value;
+            ret.add( array.length==1 ?  (Object)array[0] : array);
+        }
+        ret.addAll(extraValues.values());
+        return ret;
+    }
+
+    public Set<Entry> entrySet() {
+        Set<Entry> ret = context.getRequest().getParameterMap().entrySet();
+        ret.addAll(extraValues.entrySet());
+        return ret;
     }
 
     protected static class AutoFetchInfos {
