73863f8b431697b941a0474a6a680293c56becf7
clood <clood@aef81ea2-f21c-0410-b0e4-e2c0261c1d7f>
2006-11-08 18:31:18 +0000

    ant-vpp example

---
+++ trunk/examples/ant-vpp/demo.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+if [ "$0" == "./demo.sh" ] ; then
+
+  if [ ! -e lib/commons-collections-3.2.jar ]; then cp ../../lib/commons-collections-3.2.jar lib; fi
+  if [ ! -e lib/commons-lang-2.2.jar ]; then cp ../../lib/commons-lang-2.2.jar lib; fi
+  if [ ! -e lib/crimson-1.1.3.jar ]; then cp ../../lib/crimson-1.1.3.jar lib; fi
+  if [ ! -e lib/hsqldb-1.8.0.5.jar ]; then cp ../../test/lib/hsqldb-1.8.0.5.jar lib; fi
+  if [ ! -e lib/jdom-1.0.jar ]; then cp ../../lib/jdom-1.0.jar lib; fi
+  if [ ! -e lib/velocity-1.4.jar ]; then cp ../../lib/velocity-1.4.jar lib; fi
+  if [ ! -e lib/oro-2.0.8.jar ]; then
+    echo Downloading oro-2.0.8.jar....
+    wget http://www.ibiblio.org/maven/oro/jars/oro-2.0.8.jar -O lib/oro-2.0.8.jar;
+  fi
+ 
+  cp ../../velosurf-*.jar lib
+
+  OLDCLASSPATH=$CLASSPATH
+  for lib in lib/*.jar; do CLASSPATH=$CLASSPATH:$lib; done;
+  echo CLASSPATH=$CLASSPATH
+  export CLASSPATH=$CLASSPATH
+  ant demo
+  CLASSPATH=$OLDCLASSPATH
+  export CLASSPATH
+
+else
+  echo "demo.sh must be launched in its directory!"
+fi
+
