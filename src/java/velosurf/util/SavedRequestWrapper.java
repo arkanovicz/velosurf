79d7fb0948fad91cfe154c1e62255659f5cd188a
Claude Brisson <arkanovicz@users.noreply.github.com>
2006-11-29 16:11:29 +0000

    query string is now properly restored

--- src/java/velosurf/util/SavedRequestWrapper.java
+++ src/java/velosurf/util/SavedRequestWrapper.java
@@ -27,6 +27,7 @@ import java.util.*;
  * <p>This class provides request parameters, headers, cookies from original requrest or saved request.</p>
  *
  * @author Andrey Grebnev <a href="mailto:andrey.grebnev@blandware.com">&lt;andrey.grebnev@blandware.com&gt;</a>
+ * @author <a href="mailto:claude.brisson@gmail.com">Claude Brisson</a>
  */
 
 public @SuppressWarnings("deprecation") class SavedRequestWrapper extends HttpServletRequestWrapper {
@@ -54,33 +55,26 @@ public @SuppressWarnings("deprecation") class SavedRequestWrapper extends HttpSe
      * Constructor
      * @param request to save
      */
-    public SavedRequestWrapper(HttpServletRequest request) {
+    public SavedRequestWrapper(HttpServletRequest request,SavedRequest saved) {
         super(request);
 
         HttpSession session = request.getSession(false);
         if (session == null)
             return;
 
-        SavedRequest saved = (SavedRequest)session.getAttribute("velosurf.auth.saved-request");
-        if (saved == null)
-            return;
-
         String requestURI = saved.getRequestURI();
         if (requestURI == null)
             return;
 
-        if (requestURI.equals(request.getRequestURI())) {
-            savedRequest = saved;
-            session.removeAttribute("velosurf.auth.saved-request");
+        savedRequest = saved;
 
-            formats[0] = new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss zzz", Locale.US);
-            formats[1] = new SimpleDateFormat("EEEEEE, dd-MMM-yy HH:mm:ss zzz", Locale.US);
-            formats[2] = new SimpleDateFormat("EEE MMMM d HH:mm:ss yyyy", Locale.US);
+        formats[0] = new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss zzz", Locale.US);
+        formats[1] = new SimpleDateFormat("EEEEEE, dd-MMM-yy HH:mm:ss zzz", Locale.US);
+        formats[2] = new SimpleDateFormat("EEE MMMM d HH:mm:ss yyyy", Locale.US);
 
-            formats[0].setTimeZone(GMT_ZONE);
-            formats[1].setTimeZone(GMT_ZONE);
-            formats[2].setTimeZone(GMT_ZONE);
-        }
+        formats[0].setTimeZone(GMT_ZONE);
+        formats[1].setTimeZone(GMT_ZONE);
+        formats[2].setTimeZone(GMT_ZONE);
     }
 
     /**
@@ -89,11 +83,7 @@ public @SuppressWarnings("deprecation") class SavedRequestWrapper extends HttpSe
      * @return the HTTP method
      */
     public String getMethod() {
-        if (savedRequest == null) {
-            return super.getMethod();
-        } else {
-            return savedRequest.getMethod();
-        }
+        return savedRequest.getMethod();
     }
 
     /**
@@ -103,17 +93,13 @@ public @SuppressWarnings("deprecation") class SavedRequestWrapper extends HttpSe
      * @return header value or null
      */
     public String getHeader(String name) {
-        if (savedRequest == null) {
-            return super.getHeader(name);
-        } else {
-            String header = null;
-            Iterator iterator = savedRequest.getHeaderValues(name);
-            while (iterator.hasNext()) {
-                header = (String) iterator.next();
-                break;
-            }
-            return header;
+        String header = null;
+        Iterator iterator = savedRequest.getHeaderValues(name);
+        while (iterator.hasNext()) {
+            header = (String) iterator.next();
+            break;
         }
+        return header;
     }
 
     /**
@@ -124,15 +110,11 @@ public @SuppressWarnings("deprecation") class SavedRequestWrapper extends HttpSe
      */
 
     public int getIntHeader(String name) {
-        if (savedRequest == null) {
-            return super.getIntHeader(name);
+        String value = getHeader(name);
+        if (value == null) {
+            return (-1);
         } else {
-            String value = getHeader(name);
-            if (value == null) {
-                return (-1);
-            } else {
-                return (Integer.parseInt(value));
-            }
+            return (Integer.parseInt(value));
         }
     }
 
@@ -143,20 +125,16 @@ public @SuppressWarnings("deprecation") class SavedRequestWrapper extends HttpSe
      * @return date header value or null
      */
     public long getDateHeader(String name) {
-        if (savedRequest == null) {
-            return super.getDateHeader(name);
-        } else {
-            String value = getHeader(name);
-            if (value == null)
-                return (-1L);
+        String value = getHeader(name);
+        if (value == null)
+            return (-1L);
 
-            // Attempt to convert the date header in a variety of formats
-            long result = FastHttpDateFormat.parseDate(value, formats);
-            if (result != (-1L)) {
-                return result;
-            }
-            throw new IllegalArgumentException(value);
+        // Attempt to convert the date header in a variety of formats
+        long result = FastHttpDateFormat.parseDate(value, formats);
+        if (result != (-1L)) {
+            return result;
         }
+        throw new IllegalArgumentException(value);
     }
 
     /**
@@ -166,11 +144,7 @@ public @SuppressWarnings("deprecation") class SavedRequestWrapper extends HttpSe
      */
 
     public Enumeration getHeaderNames() {
-        if (savedRequest == null) {
-            return super.getHeaderNames();
-        } else {
-            return new Enumerator(savedRequest.getHeaderNames());
-        }
+        return new Enumerator(savedRequest.getHeaderNames());
     }
 
 
@@ -181,11 +155,7 @@ public @SuppressWarnings("deprecation") class SavedRequestWrapper extends HttpSe
      * @return enumeration of values for this header
      */
     public Enumeration getHeaders(String name) {
-        if (savedRequest == null) {
-            return super.getHeaders(name);
-        } else {
-            return new Enumerator(savedRequest.getHeaderValues(name));
-        }
+        return new Enumerator(savedRequest.getHeaderValues(name));
     }
 
     /**
@@ -194,12 +164,8 @@ public @SuppressWarnings("deprecation") class SavedRequestWrapper extends HttpSe
      * @return cookies array
      */
     public Cookie[] getCookies() {
-        if (savedRequest == null) {
-            return super.getCookies();
-        } else {
-            List cookies = savedRequest.getCookies();
-            return (Cookie[])cookies.toArray(new Cookie[cookies.size()]);
-        }
+        List cookies = savedRequest.getCookies();
+        return (Cookie[])cookies.toArray(new Cookie[cookies.size()]);
     }
 
     /**
@@ -209,11 +175,7 @@ public @SuppressWarnings("deprecation") class SavedRequestWrapper extends HttpSe
      * @value array of values
      */
     public String[] getParameterValues(String name) {
-        if (savedRequest == null) {
-            return super.getParameterValues(name);
-        } else {
-            return savedRequest.getParameterValues(name);
-        }
+        return savedRequest.getParameterValues(name);
     }
 
     /**
@@ -223,11 +185,7 @@ public @SuppressWarnings("deprecation") class SavedRequestWrapper extends HttpSe
      */
 
     public Enumeration getParameterNames() {
-        if (savedRequest == null) {
-            return super.getParameterNames();
-        } else {
-            return new Enumerator(savedRequest.getParameterNames());
-        }
+        return new Enumerator(savedRequest.getParameterNames());
     }
 
     /**
@@ -236,11 +194,7 @@ public @SuppressWarnings("deprecation") class SavedRequestWrapper extends HttpSe
      * @return parameter map
      */
     public Map getParameterMap() {
-        if (savedRequest == null) {
-            return super.getParameterMap();
-        } else {
-            return savedRequest.getParameterMap();
-        }
+        return savedRequest.getParameterMap();
     }
 
     /**
@@ -251,38 +205,12 @@ public @SuppressWarnings("deprecation") class SavedRequestWrapper extends HttpSe
      */
 
     public String getParameter(String name) {
-/*
-        if (savedRequest == null) {
-            return super.getParameter(name);
-        } else {
-            String value = null;
-            String[] values = savedRequest.getParameterValues(name);
-            if (values == null)
-                return null;
-            for (int i = 0; i < values.length; i++) {
-                value = values[i];
-                break;
-            }
-            return value;
-        }
-*/
-        //we do not get value from super.getParameter because there is a bug in Jetty servlet-container
-        String value = null;
-        String[] values = null;
-        if (savedRequest == null) {
-            values = super.getParameterValues(name);
-        } else {
-            values = savedRequest.getParameterValues(name);
-        }
-
-        if (values == null)
+        String[] values = savedRequest.getParameterValues(name);
+        if (values == null) {
             return null;
-        for (int i = 0; i < values.length; i++) {
-            value = values[i];
-            break;
+        } else {
+            return values[0];
         }
-        return value;
-
     }
 
 
@@ -293,17 +221,13 @@ public @SuppressWarnings("deprecation") class SavedRequestWrapper extends HttpSe
      */
 
     public Enumeration getLocales() {
-        if (savedRequest == null) {
-            return super.getLocales();
+        Iterator iterator = savedRequest.getLocales();
+        if (iterator.hasNext()) {
+            return new Enumerator(iterator);
         } else {
-            Iterator iterator = savedRequest.getLocales();
-            if (iterator.hasNext()) {
-                return new Enumerator(iterator);
-            } else {
-                ArrayList results = new ArrayList();
-                results.add(defaultLocale);
-                return new Enumerator(results.iterator());
-            }
+            ArrayList results = new ArrayList();
+            results.add(defaultLocale);
+            return new Enumerator(results.iterator());
         }
     }
 
@@ -314,20 +238,16 @@ public @SuppressWarnings("deprecation") class SavedRequestWrapper extends HttpSe
      */
 
     public Locale getLocale() {
-        if (savedRequest == null) {
-            return super.getLocale();
+        Locale locale = null;
+        Iterator iterator = savedRequest.getLocales();
+        while (iterator.hasNext()) {
+            locale = (Locale) iterator.next();
+            break;
+        }
+        if (locale == null) {
+            return defaultLocale;
         } else {
-            Locale locale = null;
-            Iterator iterator = savedRequest.getLocales();
-            while (iterator.hasNext()) {
-                locale = (Locale) iterator.next();
-                break;
-            }
-            if (locale == null) {
-                return defaultLocale;
-            } else {
-                return locale;
-            }
+            return locale;
         }
     }
 }
