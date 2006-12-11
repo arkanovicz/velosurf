08ea5a3e50869fa54bb94168fa5a69c5a5882ee6
clood <clood@aef81ea2-f21c-0410-b0e4-e2c0261c1d7f>
2006-12-11 20:39:39 +0000

    needed bu the DBResourceLoader

---
+++ trunk/src/java/velosurf/util/ReaderInputStream.java
@@ -0,0 +1,23 @@
+package velosurf.util;
+
+import java.io.Reader;
+import java.io.InputStream;
+import java.io.IOException;
+
+/**
+ * Simple class to allow casting a reader in an input stream.
+ *
+ *  @author <a href=mailto:claude.brisson@gmail.com>Claude Brisson</a>
+ */
+public class ReaderInputStream extends InputStream {
+
+      private Reader reader = null;
+
+      public ReaderInputStream(Reader reader) {
+          this.reader = reader;
+      }
+
+      public int read() throws IOException {
+          return reader.read();
+      }
+  }
