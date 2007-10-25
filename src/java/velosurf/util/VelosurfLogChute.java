0181377c3fb1bc4bd28b7e9b9af070bcab41d8b8
Claude Brisson <arkanovicz@users.noreply.github.com>
2007-10-25 14:26:21 +0000

    VelosurfLogChute class

--- src/java/velosurf/util/VelosurfLogChute.java
+++ src/java/velosurf/util/VelosurfLogChute.java
@@ -1,21 +1,22 @@
 package velosurf.util;
 
+import org.apache.velocity.runtime.RuntimeServices;
 import org.apache.velocity.runtime.log.LogChute;
 
 public class VelosurfLogChute implements LogChute {
 
-    void init(RuntimeServices rs) throws Exception {
+    public void init(RuntimeServices rs) throws Exception {
     }
 
-    void log(int level, String message) {
+    public void log(int level, String message) {
         Logger.log(level+1,message);
     }
 
-    void log(int level, String message, Throwable t) {
+    public void log(int level, String message, Throwable t) {
         Logger.log(message,t);
     }
 
-    boolean isLevelEnabled(int level) {
+    public boolean isLevelEnabled(int level) {
         return Logger.getLogLevel() -1 <= level;
     }
 
