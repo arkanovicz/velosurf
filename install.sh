bde7ccded4e933a95df26fd3e0142156b48ffea9
aliallaoui <ali.allaoui@gmail.com>
2018-08-06 11:50:10 +0200

    finish merge

--- install.sh
+++ install.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
 
-<<<<<<< HEAD
+
 mvn -DskipTests install:install-file -Dfile=velosurf-2.4.6.jar -DpomFile=pom.xml
-=======
-mvn -DskipTests install:install-file -Dfile=velosurf-2.4.4.jar -DpomFile=pom.xml
->>>>>>> a7c73d4db96ec1e3ce9a661613127e09078994cf
